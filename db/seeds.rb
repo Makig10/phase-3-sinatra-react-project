d11f2870732a84de9f1446917d2e5f79606d021b
Brian <mahuibrian14@gmail.com>
2023-06-07 21:25:09 +0300

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,63 @@
-puts "🌱 Seeding spices..."
+require 'faker'
+puts "🌱 Seeding data..."
 
-# Seed your database here
+  # Seed your database here
+def generate_patient_data(count)
+    patients = []
+    
+    count.times do
+      name = Faker::Name.name
+      age = Faker::Number.between(from: 1, to: 110)
+      condition = Faker::Boolean.boolean ? 'Mild' : 'Critical'
+      sex = Faker::Gender.binary_type.capitalize
+      
+      patient = {
+        name: name,
+        age: age,
+        condition: condition,
+        sex: sex
+      }
+      
+      patients << patient
+    end
+    
+    patients
+   end
+  
+  # Generate data for 200 patients
+  patients_data = generate_patient_data(200)
+  
+  # Print the generated data
+  patients_data.each do |patient|
+    puts "Name: #{patient[:name]}, Age: #{patient[:age]}, Condition: #{patient[:condition]}, Sex: #{patient[:sex]}"
+end
+
+def generate_doctor_data(count)
+    doctors = []
+    
+    count.times do
+      name = Faker::Name.name
+      rating = rand(1..5)
+      sex = Faker::Gender.binary_type.capitalize
+      
+      doctor = {
+        name: name,
+        rating: rating,
+        sex: sex
+      }
+      
+      doctors << doctor
+    end
+    
+    doctors
+  end
+  
+  # Generate data for 50 doctors
+  doctors_data = generate_doctor_data(50)
+  
+  # Print the generated data
+  doctors_data.each do |doctor|
+    puts "Name: #{doctor[:name]}, Rating: #{doctor[:rating]}, Sex: #{doctor[:sex]}"
+  end
 
 puts "✅ Done seeding!"
