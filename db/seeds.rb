ad9efb451a3ca6a4e9d7b9d990378bb018c596b9
Brian <mahuibrian14@gmail.com>
2023-06-07 23:19:00 +0300

    1st get request fully operational

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,55 +1,15 @@
 require 'faker'
 puts "🌱 Seeding data..."
 
-  # Seed your database here
-def generate_patient_data(count)
-    patients = []
-    
-    count.times do
-      name = Faker::Name.name
-      age = Faker::Number.between(from: 1, to: 110)
-      condition = Faker::Boolean.boolean ? 'Mild' : 'Critical'
-      sex = Faker::Gender.binary_type.capitalize
-      
-      patient = {
-        name: name,
-        age: age,
-        condition: condition,
-        sex: sex
-      }
-      
-      patients << patient
-    end
-    
-    patients
-   end
-  
-  # Generate data for 200 patients
-  patients_data = generate_patient_data(200)
-  
-  # Print the generated data
-  patients_data.each do |patient|
-    puts "Name: #{patient[:name]}, Age: #{patient[:age]}, Condition: #{patient[:condition]}, Sex: #{patient[:sex]}"
-end
-
-def generate_doctor_data(count)
-    doctors = []
-    
-    count.times do
+  #Making data for "doctors"table in database
+  def generate_doctor_data(count)
+    count.times.map do
       name = Faker::Name.name
       rating = rand(1..5)
       sex = Faker::Gender.binary_type.capitalize
-      
-      doctor = {
-        name: name,
-        rating: rating,
-        sex: sex
-      }
-      
-      doctors << doctor
+  
+      Doctor.create(name: name, rating: rating, sex: sex)
     end
-    
-    doctors
   end
   
   # Generate data for 50 doctors
@@ -57,26 +17,45 @@ def generate_doctor_data(count)
   
   # Print the generated data
   doctors_data.each do |doctor|
-    puts "Name: #{doctor[:name]}, Rating: #{doctor[:rating]}, Sex: #{doctor[:sex]}"
+    puts "Name: #{doctor.name}, Rating: #{doctor.rating}, Sex: #{doctor.sex}"
+  end
+
+
+  #Making data for "patients" table in database
+  def generate_patient_data(count)
+    count.times.map do
+      name = Faker::Name.name
+      age = Faker::Number.between(from: 1, to: 110)
+      condition = Faker::Boolean.boolean ? 'Mild' : 'Critical'
+      sex = Faker::Gender.binary_type.capitalize
+  
+      Patient.create(name: name, age: age, condition: condition, sex: sex)
+    end
   end
   
-#Making appointment dates and durations
- appointment_dates = []
- durations = []
+  # Generate data for 200 patients
+  patients_data = generate_patient_data(200)
+  
+  # Print the generated data
+  patients_data.each do |patient|
+    puts "Name: #{patient.name}, Age: #{patient.age}, Condition: #{patient.condition}, Sex: #{patient.sex}"
+  end
 
- 200.times do
-  appointment_dates << Faker::Date.between(from: Date.today, to: Date.today + 30) # Generate random appointment date within the next 30 days
-  durations << Faker::Number.between(from: 15, to: 60) # Generate random appointment duration between 15 and 60 minutes
- end
+  #Making data for "appointments" table in database
+  appointment_dates = []
+durations = []
 
- # Seed the data into the appointments table
- 200.times do |index|
+200.times do
+  appointment_dates << Faker::Date.between(from: Date.today, to: Date.today + 30)
+  durations << Faker::Number.between(from: 15, to: 60)
+end
+
+200.times do |index|
   Appointment.create(
-    doctor_id: Faker::Number.between(from: 1, to: 50), # Randomly assign a doctor_id from 1 to 50
-    patient_id: Faker::Number.between(from: 1, to: 200), # Randomly assign a patient_id from 1 to 200
+    doctor_id: Faker::Number.between(from: 1, to: 50),
+    patient_id: Faker::Number.between(from: 1, to: 200),
     appointment_date: appointment_dates[index],
     duration: durations[index]
   )
 end
-
 puts "✅ Done seeding!"
